85caec719960d25e17dbff7cd31e0e0096904d89
Muukii <muukii.app@gmail.com>
2023-05-07 03:00:07 +0900

    Init

---
+++ Tests/SnapDraggingModifierTests/swiftui_snap_dragging_modifierTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swiftui_snap_dragging_modifier
+
+final class swiftui_snap_dragging_modifierTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
