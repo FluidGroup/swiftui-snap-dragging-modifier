a06913173c0b9de41c729cc4dcb01d267ff8bdbd
Muukii <muukii.app@gmail.com>
2025-01-31 13:03:43 +0900

    Patch

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "swiftui-snap-dragging-modifier",
-  platforms: [.iOS(.v15)],
+  platforms: [.iOS(.v16)],
   products: [
     .library(
       name: "SwiftUISnapDraggingModifier",
